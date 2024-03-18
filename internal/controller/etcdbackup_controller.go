69242cced12b272e451db0314c8d6af2cd1c8c84
Jay Chen <chenjie@chenjie.info>
2024-03-18 13:41:49 +0800

    add event role https://book-v1.book.kubebuilder.io/beyond_basics/creating_events

--- internal/controller/etcdbackup_controller.go
+++ internal/controller/etcdbackup_controller.go
@@ -50,6 +50,7 @@ type EtcdBackupReconciler struct {
 //+kubebuilder:rbac:groups=etcd.chenjie.info,resources=etcdbackups,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=etcd.chenjie.info,resources=etcdbackups/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=etcd.chenjie.info,resources=etcdbackups/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
